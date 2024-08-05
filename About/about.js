9de6b788461982b8e9c8c9f635f3acee21b974a4
josephmalete1997 <josephmalete75@gmail.com>
2024-08-05 10:11:24 +0200

    Initial commit

--- About/about.js
+++ About/about.js
@@ -1,3 +1,116 @@
 const aboutImagesDisplay = document.querySelector(".about-images");
+const overLayer = document.querySelector(".over-layer");
 
+const imagesArray = [
+  {
+    src: `../displayImages/_MG_0225.jpg`,
+    alt: `marketing Fridays images`,
+  },
+  {
+    src: `../displayImages/_MG_0122.jpg`,
+    alt: `marketing Fridays images`,
+  },
+  {
+    src: `../displayImages/_MG_8872.jpg`,
+    alt: `marketing Fridays images`,
+  },
+  {
+    src: `../displayImages/_MG_2485.jpg`,
+    alt: `marketing Fridays images`,
+  },
+  {
+    src: `../displayImages/_MG_8365.jpg`,
+    alt: `marketing Fridays images`,
+  },
+  {
+    src: `../displayImages/_MG_8432.jpg`,
+    alt: `marketing Fridays images`,
+  },
+];
 
+const imageObject = {
+  src: null,
+};
+
+const countObject = {
+  value: 0,
+};
+
+function generateImagePanels() {
+  imagesArray.forEach((item, index) => {
+    const imageItem = document.createElement("div");
+    imageItem.classList.add("about-images-item");
+    if (index % imagesArray.length === 3) {
+      imageItem.style.marginLeft = `0%`;
+    }
+    imageItem.style.backgroundImage = `url(${item.src})`;
+    imageItem.addEventListener("click", () => {
+      popUpScroll([item.src, index]);
+    });
+    aboutImagesDisplay.append(imageItem);
+  });
+}
+
+generateImagePanels();
+
+function popUpScroll(arg) {
+  overLayer.innerHTML = "";
+  imageObject.src = arg[0];
+  imageObject.index = arg[1];
+  const close = document.createElement("div");
+  close.classList.add("close");
+  const left = document.createElement("div");
+  const right = document.createElement("div");
+  const text = document.createElement("h2");
+  const textCopyRight = document.createElement("p");
+  textCopyRight.innerHTML = `Copyright &copy;${new Date().getFullYear()} | <b>Marketing Fridays</b>`;
+  text.innerHTML = `${imageObject.index + 1} / ${imagesArray.length}`;
+  text.classList.add("scroll-text");
+  textCopyRight.classList.add("text-copy-right");
+  left.classList.add("left");
+  right.classList.add("right");
+
+  close.innerHTML = timesSign;
+  right.innerHTML = rightChevron;
+  left.innerHTML = leftChevron;
+
+  const image = document.createElement("div");
+  image.classList.add("pop-up-image-scroll");
+  image.style.backgroundImage = `url(${imageObject.src})`;
+  image.append(text, textCopyRight);
+  overLayer.append(image, close, left, right);
+  overLayer.style.display = "flex";
+  document.body.style.overflow = "hidden";
+  countObject.value = imageObject.index;
+  activateSlide();
+}
+
+function removeChildren(parent, children) {}
+function activateSlide() {
+  const close = document.querySelector(".close");
+  const left = document.querySelector(".left");
+  const right = document.querySelector(".right");
+  const imageScroll = document.querySelector(".pop-up-image-scroll");
+  const text = document.querySelector(".scroll-text");
+
+  close.addEventListener("click", () => {
+    overLayer.style.display = "none";
+    document.body.style.overflow = "auto";
+    imageObject.src = 0;
+    countObject.value = 0;
+  });
+
+  right.addEventListener("click", () => {
+    countObject.value = countObject.value + 1;
+    if (countObject.value > imagesArray.length - 1) countObject.value = 0;
+    text.innerHTML = `${countObject.value + 1} / ${imagesArray.length}`;
+    imageScroll.style.backgroundImage = `url(${imagesArray[countObject.value].src})`;
+  });
+
+  left.addEventListener("click", () => {
+    countObject.value = countObject.value - 1;
+    if (countObject.value < 0) countObject.value = imagesArray.length - 1;
+    text.innerHTML = `${countObject.value + 1} / ${imagesArray.length}`;
+    imageScroll.style.backgroundImage = `url(${imagesArray[countObject.value].src})`;
+  });
+}
---
+++ About/svgs.js
@@ -0,0 +1,9 @@
+const timesSign = `
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" width='50' height='50' fill='white'><!--! Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) Copyright 2023 Fonticons, Inc. --><path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/></svg>
+`;
+
+const rightChevron = `
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"  width='50' height='50' fill='white'><!--! Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) Copyright 2023 Fonticons, Inc. --><path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z"/></svg>`;
+
+const leftChevron = `
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"  width='50' height='50' fill='white'><!--! Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) Copyright 2023 Fonticons, Inc. --><path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z"/></svg>`;
